5afc328869552d7ae28258678131bc937f8d4874
Mustafa Kanat <mkanat6620@gmail.com>
2024-04-07 17:42:00 +0300

    v13

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { toast } from "react-toastify"
 export const registerAction = (authData) => async(dispatch) =>{
     try {
-        const data = await axios.post("http://localhost:5002/register", authData)
+        const data = await axios.post("https://post-app-bay.vercel.app/register", authData)
         dispatch({type:"REGISTER", payload: data})
         window.location = "/"
     } catch (error) {
@@ -12,7 +12,7 @@ export const registerAction = (authData) => async(dispatch) =>{
 
 export const loginAction = (authData) => async(dispatch) =>{
     try {
-        const data = await axios.post("http://localhost:5002/login", authData)
+        const data = await axios.post("https://post-app-bay.vercel.app/login", authData)
         dispatch({type:"LOGIN", payload: data})
         window.location = "/"
     } catch (error) {
--- client/src/redux/actions/post.js
+++ client/src/redux/actions/post.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 export const getPostsAction = () => async(dispatch) =>{
     try {
-        const data = await axios.get("http://localhost:5002/getPosts")
+        const data = await axios.get("https://post-app-bay.vercel.app/getPosts")
         dispatch({type:"GET_POSTS", payload: data.data})
     } catch (error) {
         console.log(error);
@@ -10,7 +10,7 @@ export const getPostsAction = () => async(dispatch) =>{
 
 export const deletePostAction = (id) => async(dispatch) =>{
     try {
-        await axios.delete(`http://localhost:5002/deletepost/${id}`)
+        await axios.delete(`https://post-app-bay.vercel.app/deletepost/${id}`)
 
         dispatch({type:"DELETE_POST", payload: id})
 
@@ -22,7 +22,7 @@ export const deletePostAction = (id) => async(dispatch) =>{
 
 export const createPostAction = (postData) => async(dispatch) =>{
     try {
-        const data = await axios.post("http://localhost:5002/createpost",postData)
+        const data = await axios.post("https://post-app-bay.vercel.app/createpost",postData)
         console.log(data.data);
         dispatch({type:"CREATE_POST", payload: data.data})
     } catch (error) {
@@ -32,7 +32,7 @@ export const createPostAction = (postData) => async(dispatch) =>{
 
 export const updatePostAction = (id,postData) => async(dispatch) =>{
     try {
-        const data = await axios.patch(`http://localhost:5002/updatepost/${id}`, postData)
+        const data = await axios.patch(`https://post-app-bay.vercel.app/updatepost/${id}`, postData)
         dispatch({type:"UPDATE_POST", payload: data.data})
     } catch (error) {
         console.log(error);
